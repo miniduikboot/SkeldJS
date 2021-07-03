56e42887107acd1c00b22e852b5840cedfd95391
miniduikboot <mini@duikbo.at>
2021-07-03 12:30:00 +0200

    Add Camera Positions
    These were obtained by dumping values from the game

---
+++ packages/data/lib/Cameras.ts
@@ -0,0 +1,96 @@
+import { GameMap } from "@skeldjs/constant";
+
+export const CameraPositions = {
+    [GameMap.TheSkeld]: [
+        {
+            // Admin Hallway
+            x: 0.71440005,
+            y: -1.6000004,
+        },
+        {
+            // Weapons/Oxygen/Navigation/Shields
+            x: 12.789999,
+            y: -3.086,
+        },
+        {
+            // Medbay
+            x: -6.8848,
+            y: 2.9648001,
+        },
+        {
+            // Reactor/Security Hallway
+            x: -17.879202,
+            y: -4.3176,
+        },
+    ],
+    [GameMap.MiraHQ]: [],
+    [GameMap.Polus]: [
+        {
+            // East
+            x: 28.992,
+            y: -15.577,
+        },
+        {
+            // Central
+            x: 15.539,
+            y: -15.586,
+        },
+        {
+            // Northeast
+            x: 24.517,
+            y: -8.534,
+        },
+        {
+            // South
+            x: 17.155,
+            y: -20.412,
+        },
+        {
+            // Southwest
+            x: 4.717,
+            y: -22.488,
+        },
+        {
+            // Northwest
+            x: 11.49,
+            y: -8.274,
+        },
+    ],
+    [GameMap.Airship]: [
+        {
+            // Engine Room
+            x: -8.420999,
+            y: 0.006999993,
+        },
+        {
+            // Vault
+            x: -3.910199,
+            y: 9.558499,
+        },
+        {
+            // Records Left
+            x: 16.6138,
+            y: 10.3075,
+        },
+        {
+            // Records Right
+            x: 23.7528,
+            y: 10.3005,
+        },
+        {
+            // Security
+            x: 4.7355,
+            y: -11.1503,
+        },
+        {
+            // Cargo Bay
+            x: 30.044,
+            y: -0.37799996,
+        },
+        {
+            // Meeting Room
+            x: 3.087,
+            y: 16.695,
+        },
+    ],
+};
